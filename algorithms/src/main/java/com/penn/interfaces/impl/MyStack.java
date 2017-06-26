d66293d3908f0894dcb737c5d3ecc4381ee84c56
zhongpingtang <you@example.com>
2017-06-27 07:56:18 +0800

    algorithms-demonstrate code example modify

--- algorithms/src/main/java/com/penn/interfaces/impl/MyStack.java
+++ algorithms/src/main/java/com/penn/interfaces/impl/MyStack.java
@@ -10,7 +10,70 @@ public class MyStack {
     private Stack<Integer> dataStack = new Stack<Integer>();/*数据栈*/
     private Stack<Integer> minStack = new Stack<Integer>();/*最小值栈*/
 
-    //TODO：完成剩下的逻辑
+
+    /**
+     * 添加value
+     * @param value
+     */
+    public void putData(Integer value){
+        /**
+         * 这段代码的问题在于：先存大数，然后再存小数，然后逐个pop，到小数pop完之后，会没有最小值
+         * 这明显是不合理的
+
+        boolean minstackAddFlage=true;
+        if(!dataStack.isEmpty()&&minStack.peek().compareTo(value)<0){
+            minstackAddFlage = false;
+        }else if(!minStack.isEmpty()&&minStack.peek().compareTo(value)>0){
+            minStack.pop();
+        }
+        //最终添加
+        if (minstackAddFlage) {
+            //保持最小值
+            minStack.add(value);
+        }
+         */
+        //example demonstrate are here：
+        if (dataStack.isEmpty()||getMin()>=value) {
+            minStack.add(value);
+        }
+        //加入数据栈
+        dataStack.add(value);
+    }
+
+
+
+    public void popData(){
+        if (dataStack.isEmpty()) {
+            System.out.println("数据栈为空");
+            return ;
+        }
+        Integer value = dataStack.pop();
+        if (value.equals(getMin())) {
+            minStack.pop();
+        }
+
+
+    }
+
+    /**
+     * 获取最小值的操作
+     */
+    public Integer getMin(){
+        if (minStack.isEmpty()) {
+            System.out.println("the minest is:"+minStack.peek());
+            throw new RuntimeException("数据栈为空");
+        }
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MyStack myStack = new MyStack();
+        myStack.putData(1);
+        myStack.putData(4);
+        myStack.putData(35);
+        System.out.println(myStack.getMin());
+
+    }
 
 
 
